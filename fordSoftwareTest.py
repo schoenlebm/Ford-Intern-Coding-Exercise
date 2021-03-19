2b44c01cfd53cc3271a548f5bc4c100ca9e2be3d
schoenlebm <43391573+schoenlebm@users.noreply.github.com>
2021-03-19 17:48:43 -0400

    added files via upload
    added the two exercise code files

---
+++ fordSoftwareTest.py
@@ -0,0 +1,46 @@
+#Author: Michael Schoenleb
+#Date: 3/16
+#Purpose: Ford Software Programming Assessment, a gui that accepts a user integer input, and... 
+#outputs either Mustang if that input is divisible by 3, Bronco if it is divisible by 5, MustangBronco if it is divisible by both, or just that integer if it is divisible by neither
+import tkinter as tk
+import unittest
+class mustangBroncoGUI():
+    
+    def __init__(self):
+        self.top = tk.Tk()
+        
+        # component set up
+        self.greeting = tk.Label(text="Input an integer in the entry box below, then press enter.",width=70)
+        self.greeting.pack(pady=4)
+        
+        self.entry = tk.Entry(width = 20)
+        self.entry.pack(pady=4)
+        
+        self.button =tk.Button(text="Enter",command=self.mustangBroncoCallBack)
+        self.button.pack(pady=4)
+        
+        self.output = tk.Label()
+        self.output.pack(pady=4)
+        
+        self.top.mainloop()
+        
+        
+    def mustangBroncoCallBack(self):
+        user_input = self.entry.get()
+        try:
+            user_input = int(user_input)
+        except ValueError:
+            self.output['text'] = "ERROR: Invalid input (potentially non-integer), please try again"
+            return
+        if (user_input % 3 == 0) and (user_input % 5 == 0):
+            self.output['text'] = "MustangBronco"
+        elif user_input % 3 == 0:
+            self.output['text'] = "Mustang"
+        elif user_input % 5 == 0:
+            self.output['text'] = "Mustang"
+        else:
+            self.output['text'] = user_input
+            
+
+
+mustangBroncoGUI()
